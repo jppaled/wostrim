7e173f7c0c6bbb821243b510f0a6cf88a5c707e2
jppaled <22444128+jppaled@users.noreply.github.com>
2022-02-06 01:35:01 +0100

    open issue for testing twitch url without opening stream with mpv

--- mpv.sh
+++ mpv.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 function openInMpv() {
-    if $(mpv -msg-level https://twitch.tv/$1 | grep -q 'offline\|not exist'); then
+    # need to fix this, it does not works
+    # https://github.com/mpv-player/mpv/issues/9834
+    if $(mpv --msg-level=error=trace https://twitch.tv/$1 | grep -q 'offline\|not exist'); then
         echo "$1 is not online or does not exist"
     else
         mpv https://twitch.tv/$1
