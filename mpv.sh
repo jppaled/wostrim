d38a2883026834b7a276d0d6c6abaa1313e17c2c
jppaled <22444128+jppaled@users.noreply.github.com>
2022-02-06 16:24:59 +0100

    improve check twitch url speed thanks to @guidocella

--- mpv.sh
+++ mpv.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 function openInMpv() {
-    if $(mpv "https://www.twitch.tv/$1" --end=0.1 --vo=null --keep-open=no --msg-level=all=error | grep -q 'offline\|not exist'); then
+    if $(youtube-dl -sq "https://twitch.tv/$1" | grep -q 'offline\|not exist'); then
         echo -e "$1 is not online or does not exist"
     else
         echo -e "The stream will start soon..."
